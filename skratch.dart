0d6ed58cda916b2b83dc34edfeef8e9bbcbba74e
Karim Elbayaa <kareemelbayaa@gmail.com>
2020-10-02 00:02:06 +0200

    Adding Aync programming library

---
+++ skratch.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+void main()
+{
+  performTasks();
+}
+
+void performTasks() async{
+  task1(1);
+  //task 3 depends on what comes from task 2
+  String task2Data = await task2(2);
+  task3(task2Data,3);
+}
+
+void task1(int taskOrder) {
+  print('task$taskOrder is simple synchronous programming');
+}
+
+Future task2(int taskOrder) async{
+  //TODO 1.0 async programming Future.delayed(duration,(){})
+  Duration duration = Duration(seconds: 3);
+  String result;
+
+  await Future.delayed(duration,(){
+    result = 'result';
+    print('task$taskOrder data is: $result');
+  });
+  return result;
+}
+
+void task3(String resultFromTask2,int taskOrder) {
+  print('task$taskOrder result from task2 is: $resultFromTask2');
+}
+
+
